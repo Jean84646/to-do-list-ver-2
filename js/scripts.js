8bb5f2c16d229a05ee5b457e9e39e190a3edac9c
Anousone Kaseumsouk and Lan Dam <anousone@>
2018-06-11 11:30:53 -0700

    add scripts to add task to to do list when you submit the task form

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,15 @@ $(document).ready(function(){
   $('button#add-list-item').click(function(){
     toDoCounter += 1;
     $('#todo-list-input').append('<input id="todo' + toDoCounter + '" type="text" class="form-control">');
-    
+
+  });
+
+  $('#task-form').submit(function(event){
+    event.preventDefault();
+    var taskInput = $('input#input-task').val();
+    var newTask = new toDoList(taskInput);
+
+    $('#todo-list').append('<li><span class="todo-task">' + newTask.task + '</span></li>');
   });
 
 });
