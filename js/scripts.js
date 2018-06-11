d42ca68fafd48114d2d82fd9566aa9d06e92a937
Anousone Kaseumsouk and Lan Dam <anousone@>
2018-06-11 11:23:45 -0700

    add button to add addition entries for todo list

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,12 @@ function toDoList(task){
 
 
 // user interface logic
+$(document).ready(function(){
+  var toDoCounter = 1;
+  $('button#add-list-item').click(function(){
+    toDoCounter += 1;
+    $('#todo-list-input').append('<input id="todo' + toDoCounter + '" type="text" class="form-control">');
+    
+  });
+
+});
